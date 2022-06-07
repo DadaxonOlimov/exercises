aefd9fcd0739885ac8e4bf87576de289bec72794
DadaxonOlimov <dadaxonolimov707@gmail.com>
2022-06-07 19:36:10 +0500

    2%

--- gilam/index.py
+++ gilam/index.py
@@ -1,6 +1,7 @@
-
-  
 # Direktor tashrifi
-# Tez orada RoboContest tizimining qanday ishlayotganligini tekshirish uchun RoboticsLab direktori ishxonamizga tashrif buyuradi. Biz direktorni tantanali kutib olish maqsadida direktorning moshinadan tushgan joyidan ishxonamiz eshigigacha bo'lgan oraliqga gilam to'shashga qaror qildik. Bizga ma'lumki direktorning mashinasi to'xtatiladigan joydan ishxonamiz kirish eshigigacha bo'lgan masofa NN metr, bozorda 1 metr gilamning narxi PP so'm. Biz gilam sotib olish uchun jami qancha mablag' sarflashimizni aniqlang!.
+# Tez orada RoboContest tizimining qanday ishlayotganligini tekshirish uchun RoboticsLab direktori ishxonamizga tashrif buyuradi.
+#  Biz direktorni tantanali kutib olish maqsadida direktorning moshinadan tushgan joyidan ishxonamiz eshigigacha bo'lgan oraliqga gilam to'shashga qaror qildik. 
+# Bizga ma'lumki direktorning mashinasi to'xtatiladigan joydan ishxonamiz kirish eshigigacha bo'lgan masofa NN metr, bozorda 1 metr gilamning narxi PP so'm. 
+# Biz gilam sotib olish uchun jami qancha mablag' sarflashimizni aniqlang!.
 [a,b] =list(map(int, input().split(" ")))
 print(a*b)
