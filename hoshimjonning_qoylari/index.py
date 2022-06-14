67edeef53afd3812c831de91d9801e3aae8cc422
DadaxonOlimov <dadaxonolimov707@gmail.com>
2022-06-14 21:15:45 +0500

    2%

---
+++ hoshimjonning_qoylari/index.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = a/100
+if b == int(b):
+    print(int(b))
+else:
+    print(b)
+# a = int(b) if b == int(b) else b
+# print(a)
--- taqqosla/index.py
+++
@@ -1,7 +0,0 @@
-[a,b,c] =list(map(int, input().split(" ")))
-if a<b<c:
-    print(c)
-elif a>b>c:
-    print(a)    
-else:
-    print(b)
