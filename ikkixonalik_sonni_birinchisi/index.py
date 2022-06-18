89ba5d589f052ce7b2d0df7e47480101ea21367e
DadaxonOlimov <dadaxonolimov707@gmail.com>
2022-06-18 13:47:15 +0500

    3%

---
+++ ikkixonalik_sonni_birinchisi/index.py
@@ -0,0 +1,12 @@
+# Ikki xonali sonning birinchi raqami
+# Ikki xonali son berilgan. Uning birinchi raqamini(o`nliklar sonini ) aniqlang.
+
+# Kiruvchi ma'lumotlar:
+# Kirish faylida ikki xonali natural N soni berilgan.
+
+# Chiquvchi ma'lumotlar:
+# Bitta butun son - masala javobini chiqaring.
+a= (input())
+print(int(a[0]))
+
+
