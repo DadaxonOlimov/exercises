e46111cd810b90efc5cfa0c272cd57fec2b15522
DadaxonOlimov <dadaxonolimov707@gmail.com>
2022-06-18 14:11:19 +0500

    3%

---
+++ oxirgi raqamni topish/index.py
@@ -0,0 +1,10 @@
+# Oxirgi raqam
+# Natural son berilgan,uning oxirgi raqamini toping.
+
+# Kiruvchi ma'lumotlar:
+# Kirish faylida N(1 ≤ N ≤ 109) natural soni kiritiladi.
+
+# Chiquvchi ma'lumotlar:
+# Bitta butun son - masala javobini chiqaring.
+# a = input()
+print(int(a[-1]))
