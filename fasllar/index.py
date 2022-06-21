19f81fd1fb50583bc01c968c482c967bf2409e60
DadaxonOlimov <dadaxonolimov707@gmail.com>
2022-06-21 10:20:46 +0500

    5%

--- fasllar/index.py
+++ fasllar/index.py
@@ -1,12 +1,22 @@
+# Fasllar
+# Hurmatli 1-kurs talabalari sizlarga oy raqamini beraman siz menga shu oy qaysi faslga tegishli ekanini topishdan iborat. 
+# Oy raqamini ingliz tilida nomlanishini topishingiz kerak.
+
+# Kiruvchi ma'lumotlar:
+# Kirish faylida N natural son (N≤100)(N≤100) - oy soni mavjud
+
+# Chiquvchi ma'lumotlar:
+# Chiqish faylida yoz oylari uchun “Summer”, qish oylari uchun “Winter”, bahor uchun “Spring” va kuz uchun “Autumn” qiymatini chop eting.
+#  Agar sana oyning mumkin bo'lgan qiymatiga mos kelmasa, unda bu holda siz "Error" ni chiqarishingiz kerak.
 a =int(input())
-if a<=3:
-    print('Winter')
-elif 3<a<=6:
+if 2<a<6:
     print('Spring') 
-elif 6<a<=9:
+elif 5<a<9:
     print('Summer')
-elif 9<a<=12:
+elif 8<a<12:
     print('Autumn') 
+elif a < 3 or a == 12:
+    print("Winter")
 else:
     print('Error')        
  
