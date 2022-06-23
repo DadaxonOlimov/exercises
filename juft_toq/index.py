118f87a6d11544663778075e1f283e132e9b45cf
DadaxonOlimov <dadaxonolimov707@gmail.com>
2022-06-23 16:10:38 +0500

    5%

---
+++ juft_toq/index.py
@@ -0,0 +1,32 @@
+sonlar =list(map(int, input().split(' ')))
+yig_toq = 0
+yig_juft = 0
+for i in sonlar:
+    if i % 2 == 0:
+        yig_juft += i
+    else:
+        yig_toq += i
+print(abs(yig_juft - yig_toq) )
+
+    
+
+
+
+ 
+  
+   
+
+    
+     
+      
+       
+        
+
+         
+                   
+
+ 
+
+
+
+
